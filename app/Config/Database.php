b0c4f3e1dd214584baf87e577be7ed4295b1cd3d
IlhamNaffi-12210621 <naffiilham@gmail.com>
2022-10-12 23:08:42 +0700

    .

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'db_ehotel',
+        'database' => 'db_latihan',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,6 +7,7 @@ use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use CodeIngiter\Session\Session;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -36,7 +37,7 @@ abstract class BaseController extends Controller
      * @var array
      */
     protected $helpers = [];
-
+    
     /**
      * Constructor.
      */
@@ -47,6 +48,6 @@ abstract class BaseController extends Controller
 
         // Preload any models, libraries, etc, here.
 
-        // E.g.: $this->session = \Config\Services::session();
+        // $this->session = \Config\Services::session();
     }
 }
